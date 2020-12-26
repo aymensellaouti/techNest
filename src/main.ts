88faae93e02b2072152c1695518cf63998cbd3db
aymen2782 <aymen.sellaouti@gmail.com>
2020-12-26 12:21:34 +0100

    update db c

--- src/main.ts
+++ src/main.ts
@@ -46,6 +46,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
-  await app.listen(configService.get('port') || 8080);
+  await app.listen(configService.get('port') || 3000);
 }
 bootstrap();
